594d231362ca1bb74b12085b0b9ad9d61cfaf960
danispinxo <genericpronoun@gmail.com>
2022-09-19 17:20:03 +0000

    refactoring

--- game.rb
+++ game.rb
@@ -1,41 +1,47 @@
 class Game
-  attr_accessor :current_player
 
   def initialize
     @current_player = "Player 1"
+    @player1 = Player.new(1)
+    @player2 = Player.new(2)
   end
 
   def switch_players
-    if @current_player == Player1.name
-      @current_player = Player2.name
+    if @current_player == @player1.name
+      @current_player = @player2.name
     else
-      @current_player = Player1.name
+      @current_player = @player1.name
     end
   end
 
   def announce_scores
-    puts "P1: #{Player1.lives}/3 vs. P2: #{Player2.lives}/3"
+    puts "P1: #{@player1.lives}/3 vs. P2: #{@player2.lives}/3"
+  end
+
+  def check_answer(question, ans)
+    if (question.number1.to_i + question.number2.to_i == ans)
+      puts "#{@current_player} YES! You are correct!"
+      announce_scores
+    else
+      puts "#{@current_player}: Sorry! That is incorrect"
+      if @current_player == @player1.name
+        @player1.lose_life
+      elsif @current_player == @player2.name
+        @player2.lose_life
+      end
+    end
   end
 
   def turns
-    while Player1.lives > 0 && Player2.lives > 0
+    while @player1.lives > 0 && @player2.lives > 0
       puts "-----NEW TURN-----"
+      
       question = Question.new
       puts "#{@current_player}: #{question.generate_question}"
       
       answer = gets.chomp.to_i
 
-      if (question.number1.to_i + question.number2.to_i == answer)
-        puts "#{current_player} YES! You are correct!"
-        announce_scores
-      else
-        puts "#{current_player}: Sorry! That is incorrect"
-        if current_player == Player1.name
-          Player1.lose_life
-        elsif current_player == Player2.name
-          Player2.lose_life
-        end
-      end
+      check_answer(question, answer)
 
       switch_players
 
@@ -44,10 +50,10 @@ class Game
 
   def game_over
     puts "-----GAME OVER-----"
-    if Player1.lives == 0
-      puts "Player 2 wins with a score of #{Player2.lives}/3"
-    elsif Player2.lives == 0
-      puts "Player 1 wins with a score of #{Player1.lives}/3"
+    if @player1.lives == 0
+      puts "Player 2 wins with a score of #{@player2.lives}/3"
+    elsif @player2.lives == 0
+      puts "Player 1 wins with a score of #{@player1.lives}/3"
     end
     puts "Goodbye!"
   end
--- main.rb
+++ main.rb
@@ -2,8 +2,6 @@ require "./player"
 require "./question"
 require "./game"
 
-CurrentGame = Game.new
-Player1 = Player.new(1)
-Player2 = Player.new(2)
+currentGame = Game.new
 
-puts CurrentGame.play
+puts currentGame.play
